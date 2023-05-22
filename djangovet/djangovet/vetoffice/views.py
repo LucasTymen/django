a8df26ee136a5ead3c32ca1871d561366fc66880
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-22 17:04:52 +0200

    intro_to_django_9

--- djangovet/djangovet/vetoffice/views.py
+++ djangovet/djangovet/vetoffice/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 def home(request):
   context = {"name": "Spot"}
-  return render (request, "vetoffice/home.html", context)
-# Crefrom django.
+  return render(request, 'vetoffice/home.html', context)
